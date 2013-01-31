d43446772db85fb993c3fb5e0f2c5fd70ce04050
pernyblom <perny843@hotmail.com>
2013-01-31 20:28:28 +0100

    Did some experiments with dynamic tests and test collections

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -20,6 +20,24 @@ var testsData = {
                     }
                 }
             ]
+        },
+        {
+            name: "Math 2",
+            tests: [
+                {
+                    name: "Simple Multiplication"
+                },
+                {
+                    name: "Simple Addition",
+                    template: "simpleMathDualOperation",
+                    templateParameters: {
+                        questionCount: 5,
+                        firstDomain: [1, 2, 3, [8, 12]],
+                        secondDomain: [[0, 12], [20, 30]],
+                        operations: ['addition']
+                    }
+                }
+            ]
         }
     ]
 };
@@ -28,16 +46,53 @@ var currentCollection = null;
 var currentTest = null;
 
 $(document).ready(function() {
+
+
+    var $testsCollectionsPage = $("#tests-collections-page");
+    $testsCollectionsPage.on('pageshow', function(evt, data) {
+
+        var htmlArr = [];
+
+        var collections = testsData.testCollections;
+        for (var i=0; i<collections.length; i++) {
+            var collection = collections[i];
+            htmlArr.push('<li><a href="#tests-page" data-test-collection-index="' + i + '"  >' + collection.name + '</a></li>')
+        }
+
+        var $testCollectionList = $("#test-collection-list");
+        $testCollectionList.empty().append($(htmlArr.join(""))).listview("refresh");
+
+        $testCollectionList.find("a").on('click', function() {
+            var collectionIndex = $(this).data('test-collection-index');
+            currentCollection = collections[collectionIndex];
+        });
+
+    });
+
+
     var $testsPage = $("#tests-page");
     $testsPage.on('pageshow', function(evt, data) {
-        console.log("Showing tests page");
 
-        var arr = [
-            '<li><a href="#">Hej</a></li>',
-            '<li><a href="#">Per</a></li>'
-        ];
+        if (!currentCollection) {
+            console.log("No collection is found...");
+            return;
+        }
+        var htmlArr = [];
 
-        $("#test-list").empty().append($(arr.join(""))).listview("refresh");
+        var tests = currentCollection.tests;
+        for (var i=0; i<tests.length; i++) {
+            var test = tests[i];
+            htmlArr.push('<li><a href="#testing-page" data-test-index="' + i + '" >' + test.name + '</a></li>')
+        }
+
+        var $testList = $("#test-list");
+        $testList.empty().append($(htmlArr.join(""))).listview("refresh");
+
+        $testList.find("a").on('click', function() {
+            var testIndex = $(this).data('test-index');
+            currentTest = currentCollection[testIndex];
+        });
+
     });
 
 });
